c87decedae2bb98e22c0566e790458e7ef457d58
ViniciusSXavier999 <viniciusxaviersouza11@gmail.com>
2023-02-20 14:31:29 -0300

    Estrutura inicial do projeto

---
+++ src/test/java/com/vinivx/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinivx.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
